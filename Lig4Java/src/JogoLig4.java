58c7d8b2411878cf5299f013e51e62932551eec8
Lucasrc22 <lucasrios2210@gmail.com>
2023-07-09 19:01:25 -0300

    retirando coisas desnecessárias

--- Lig4Java/src/JogoLig4.java
+++ Lig4Java/src/JogoLig4.java
@@ -2,7 +2,7 @@ public class JogoLig4 {
     public static void main(String[] args) throws Exception {
         try {
             Lig4 jogo = new Lig4();
-            boolean modoIA = true; // Set the desired mode (true for jogador1 vs IA, false for jogador1 vs jogador2)
+            boolean modoIA = true; 
             jogo.inicializar(modoIA);
         } catch (Exception e) {
             System.out.println("Ocorreu um erro durante a execução do jogo: " + e.getMessage());
